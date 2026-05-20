04ecb4f3a0d0bd5cb1ee4e6f547977de39265aac
ccckmit <ccckmit@gmail.com>
2026-05-20 11:09:19 +0800

    cartpoleVgp pass

--- gymrun.sh
+++ gymrun.sh
@@ -2,5 +2,5 @@
 # PYTHONPATH=. python world/examples/bipedalwalker_torch.py --steps 100000
 # PYTHONPATH=. uv run pytest world/tests/test_world.py -v
 # source /Users/cccuser/.venv/bin/activate
-PYTHONPATH=. python world/examples/cartpole_closed_form.py
-# PYTHONPATH=. python world/examples/cartpole_vpg.py
+# PYTHONPATH=. python world/examples/cartpole_closed_form.py
+PYTHONPATH=. python world/examples/cartpole_vpg.py
